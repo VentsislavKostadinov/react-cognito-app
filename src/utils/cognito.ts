cc5a7959eeab9b035f8267503b8b31d8bbd1f833
VentsislavKostadinov <ventsislav_kostadinov@abv.bg>
2026-05-09 13:29:45 +0300

    update getRedirectUri

--- src/utils/cognito.ts
+++ src/utils/cognito.ts
@@ -1,5 +1,3 @@
 export const getRedirectUri = (): string => {
-  return window.location.hostname.includes('local')
-    ? 'http://localhost:5173/'
-    : import.meta.env.VITE_REDIRECT_URI;
+  return `${window.location.origin}/`;
 };
